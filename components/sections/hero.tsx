cb73d01fbb6cd817c8f66826872131ed6ee0d363
adeka stanley <adekastanley2@gmail.com>
2026-06-29 21:38:02 +0100

    problem

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -4,23 +4,23 @@ import { Badge } from "../ui/badge";
 
 export default function Hero() {
 	return (
-		<div className=" bg-amber-400/10 mask-b-from-90% mask-b-to-100% ">
+		<div className=" bg-amber-400/10 mask-b-from-90% mask-b-to-100% overflow-hidden ">
 			<div className="h-screen text-center max-w-7xl mx-auto lg:min-w-7xl  gap-2 w-full flex flex-col justify-end items-center relative">
 				<div className="bg-purple-300 absolute bottom-50">
 					<div className="absolute bg-mpk-primary w-100 h-100 -z-1 rounded-full left-5    bg-radial-[at_40%_100%] opacity-50 blur-3xl from-mpk-primary via-mpk-primary  to-transparent "></div>
-					<div className="absolute bg-red-900 w-100 h-100 -z-1 right-5 blur-3xl rounded-full top bg-radial-[at_50%_100%] opacity-30 from-mpk-primary via-blue-400  to-transparent"></div>
+					<div className="absolute  w-100 h-100 -z-1 right-5 blur-3xl rounded-full top bg-radial-[at_50%_100%] opacity-30 from-mpk-primary via-blue-400  to-transparent"></div>
 				</div>
 				<div className="bg-purple-300 absolute -top-50">
 					<div className="absolute bg-mpk-primary w-100 h-100 -z-1 rounded-full left-5    bg-radial-[at_40%_100%] opacity-50 blur-3xl from-mpk-primary via-mpk-primary  to-transparent "></div>
-					<div className="absolute bg-red-900 w-100 h-100 -z-1 right-5 blur-3xl rounded-full top bg-radial-[at_50%_100%] opacity-30 from-mpk-primary via-blue-400  to-transparent"></div>
+					<div className="absolute  w-100 h-100 -z-1 right-5 blur-3xl rounded-full top bg-radial-[at_50%_100%] opacity-30 from-mpk-primary via-blue-400  to-transparent"></div>
 				</div>
 				<Badge
 					variant={"ghost"}
 					className="outline-mpk-primary/30 outline-1   font-sans uppercase text-xs rounded-4xl font-light px-4 py-3"
 				>
-					My Period Companion
+					My Period Companion.
 				</Badge>
-				<h1 className="lg:text-7xl text-5xl lg:px-40   font-sans   ">
+				<h1 className="lg:text-7xl text-5xl lg:px-40    font-sans   ">
 					A friendly period companion, always close by..
 				</h1>
 				<div className="   lg:px-70 ">
--- components/sections/problem.tsx
+++ components/sections/problem.tsx
@@ -1,8 +1,17 @@
 export default function Problem() {
 	return (
-		<div className="">
-			<h2>THE REALITY FOR MILLIONS</h2>
+		<div className="flex flex-col items-center gap-5 text-center   min-h-screen pt-10 md:pt-40">
+			<h2 className="font-sans text-3xl">THE REALITY FOR MILLIONS</h2>
 			<p>Menstruation should`t be a barrier to education, dignity, or health</p>
+
+			<div>
+				<p className="lg:px-40">
+					For millions of adolescent girls, managing a natural monthly cycle
+					comes with silent challenges. Due to a lack of clear information,
+					social taboos, and systemic gaps, navigating the first period is often
+					a source of anxiety rather than a milestone of growth.
+				</p>
+			</div>
 		</div>
 	);
 }
