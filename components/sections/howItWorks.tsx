b5c3db56a4c7841dd37471af10bcde6982a88ebf
adeka stanley <adekastanley2@gmail.com>
2026-07-04 10:04:09 +0100

    update

--- components/sections/howItWorks.tsx
+++ components/sections/howItWorks.tsx
@@ -1,6 +1,6 @@
 export default function HowItWorks() {
 	return (
-		<div className="flex flex-col lg:flex-row max-md:items-center font-light min-h-100 lg:min-h-120 relative ">
+		<div className="flex flex-col lg:flex-row max-md:items-center font-light py-20  min-h-100 lg:min-h-120 relative ">
 			<div className="w-full  ">
 				<h2 className="lg:text-5xl text-4xl  max-md:text-center lg:px-20 ">
 					SEAMLESS ACCESS
@@ -17,9 +17,9 @@ export default function HowItWorks() {
 					using offline SMS, or scanning a physical toolkit.
 				</p>
 				<svg
-					className="absolute -z-1 blur-2xl"
+					className="absolute -z-1 blur-2xl h-200 w-auto"
 					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 10 50 150"
+					viewBox="0 10 50 100"
 				>
 					<path
 						d="M 43.63 24.79 C 44.48 29.41 47.75 32.63 45.68 36.89 C 43.99 40.37 38.99 41.00 34.99 42.57 C 31.91 43.78 28.40 45.23 24.73 45.12 C 21.52 45.02 18.09 43.89 14.80 41.98 C 11.89 40.28 9.66 38.58 7.21 34.91 C 4.79 31.28 2.01 29.22 3.34 24.79 C 4.42 21.22 10.27 22.50 12.39 17.67 C 14.24 13.46 9.88 6.91 12.58 3.76 C 15.70 0.12 21.04 2.31 24.73 3.26 C 28.97 4.35 30.42 6.88 33.74 9.18 C 36.62 11.19 39.82 12.15 41.64 15.03 C 43.58 18.09 42.79 20.27 43.63 24.79 Z"
